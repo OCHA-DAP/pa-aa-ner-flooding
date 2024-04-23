05c52be636830a3d3d753bcea848718efa8ba5fb
t-downing <downing.tristan@gmail.com>
2024-04-22 21:25:55 -0500

    floodscan exposure

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,11 @@
-ISO3 = ner  # noqa: F821
+# ISO3 = ner  # noqa: F821
+
+# admin1 - région
+NIAMEY = "NE008"
+TILLABERI = "NE006"
+DOSSO = "NE003"
+
+ADM1_AOI_PCODES = [DOSSO, TILLABERI, NIAMEY]
+
+# admin3 - commune
+GAYA = "NE003006003"
